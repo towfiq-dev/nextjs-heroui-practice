8c6c6fb825e3be801d395d99d2370f1b00ae305c
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-21 12:37:58 +0600

    Auto-update on Tue, Apr 21, 2026 12:37:58 PM

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,3 +1,4 @@
+import AddTask from '@/components/addTask/AddTask';
 import Task from '@/components/task/Task';
 import { getTasks } from '@/lib/task';
 import React from 'react';
@@ -7,6 +8,9 @@ const tasksPage = async() => {
   return (
     <div className='max-w-300 mx-auto'>
       <h1 className='text-center text-3xl font-bold my-6'>Total Length: {tasks.length}</h1>
+      <div className='flex justify-center mb-5'>
+        <AddTask></AddTask>
+      </div>
       <div className='grid grid-cols-3 justify-center items-center gap-8'>
         {
           tasks.map((task, index)=> 
